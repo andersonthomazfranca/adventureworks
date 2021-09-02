6262736f086d2a47695c48674c46cf4f073cb0ba
anderson.franca2212@gmail.com <anderson.franca2212@gmail.com>
2021-09-02 08:46:44 +0000

    Finalizei as tabelas staging

--- models/staging/stg_salesperson.sql
+++ models/staging/stg_salesperson.sql
@@ -1,5 +1,14 @@
 with dados_fonte as (
-    select *
+    select
+        row_number() over (order by businessentityid) as sk_func_vendas, -- chave autoincremental
+        businessentityid as id_func_vendas,
+        territoryid as id_territorio,
+        salesquota as cota_venda,
+        salesytd as vendas_acumulado_ano,
+        saleslastyear as vendas_ultimo_ano,
+        commissionpct as comissao,
+        bonus,
+        modifieddate as data_modificacao
     from {{ source('adventureworkserp', 'salesperson')}}
 )
 select * from dados_fonte
